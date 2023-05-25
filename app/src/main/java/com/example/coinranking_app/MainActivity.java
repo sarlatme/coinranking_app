eb307f4c10fb0280cdab4afbc8aa683405e85ed5
msarlat <meven.sarlat@capgemini.com>
2023-05-25 14:07:49 +0200

    feature(coinranking) : add a scrollview to print our coin

--- app/src/main/java/com/example/coinranking_app/MainActivity.java
+++ app/src/main/java/com/example/coinranking_app/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.coinranking_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
+import com.example.coinranking_app.models.Coin;
+import com.example.coinranking_app.models.CoinsList;
 import com.example.coinranking_app.viewModels.IViewModel;
 import com.example.coinranking_app.viewModels.RetrofitViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private IViewModel viewModel;
+    private TextView text_view_data;
+
+    private ScrollView scrollview_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +28,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewModel = new ViewModelProvider(this).get(RetrofitViewModel.class);
-        viewModel.generateListCoins();
 
+
+        text_view_data = findViewById(R.id.textview_data);
+        scrollview_text = findViewById(R.id.scrollview_text);
+
+        viewModel.getData().observe(this, new Observer<CoinsList>() {
+            @Override
+            public void onChanged(CoinsList coinsList) {
+                if (coinsList != null) {
+                    List<Coin> coins = coinsList.getCoins();
+
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    for (Coin coin : coins) {
+                        String coinName = coin.getName();
+                        double coinPrice = coin.getPrice();
+
+                        stringBuilder.append("Name: ").append(coinName).append("\n");
+                        stringBuilder.append("Coin Price: ").append(coinPrice).append("\n");
+                        stringBuilder.append("---------------------").append("\n");
+                    }
+
+                    text_view_data.setText(stringBuilder.toString());
+
+                    scrollview_text.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            scrollview_text.fullScroll(ScrollView.FOCUS_UP);
+                        }
+                    });
+                }
+            }
+        });
+
+        viewModel.generateListCoins();
 
     }
 }
