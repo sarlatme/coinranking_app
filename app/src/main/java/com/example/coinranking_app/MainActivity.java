a67f443bb119d473a309476f48579b957d376e37
msarlat <meven.sarlat@capgemini.com>
2023-05-25 16:38:20 +0200

    feature(coinranking) : add recycler view print

--- app/src/main/java/com/example/coinranking_app/MainActivity.java
+++ app/src/main/java/com/example/coinranking_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.coinranking_app;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ScrollView;
@@ -13,51 +15,39 @@ import com.example.coinranking_app.models.CoinsList;
 import com.example.coinranking_app.viewModels.IViewModel;
 import com.example.coinranking_app.viewModels.RetrofitViewModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private IViewModel viewModel;
-    private TextView text_view_data;
 
-    private ScrollView scrollview_text;
+    private RecyclerView recycler_view_coins;
+    private RecyclerAdapterCoin recycler_adapter_coin;
+    private RetrofitViewModel retrofit_view_model;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recycler_view_coins = findViewById(R.id.recyclerview_coins);
+
+        List<Coin> empty_list = new ArrayList<>();
+        recycler_adapter_coin = new RecyclerAdapterCoin(empty_list);
+
+        recycler_view_coins.setLayoutManager(new LinearLayoutManager(this));
+        recycler_view_coins.setAdapter(recycler_adapter_coin);
+
         viewModel = new ViewModelProvider(this).get(RetrofitViewModel.class);
-
-
-        text_view_data = findViewById(R.id.textview_data);
-        scrollview_text = findViewById(R.id.scrollview_text);
 
         viewModel.getData().observe(this, new Observer<CoinsList>() {
             @Override
             public void onChanged(CoinsList coinsList) {
                 if (coinsList != null) {
-                    List<Coin> coins = coinsList.getCoins();
-
-                    StringBuilder stringBuilder = new StringBuilder();
-
-                    for (Coin coin : coins) {
-                        String coinName = coin.getName();
-                        double coinPrice = coin.getPrice();
-
-                        stringBuilder.append("Name: ").append(coinName).append("\n");
-                        stringBuilder.append("Coin Price: ").append(coinPrice).append("\n");
-                        stringBuilder.append("---------------------").append("\n");
-                    }
-
-                    text_view_data.setText(stringBuilder.toString());
-
-                    scrollview_text.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            scrollview_text.fullScroll(ScrollView.FOCUS_UP);
-                        }
-                    });
+                    recycler_adapter_coin.setCoinList(coinsList.getCoins());
+                    recycler_adapter_coin.notifyItemRangeInserted(0,coinsList.getCoins().size());
                 }
             }
         });
@@ -65,4 +55,6 @@ public class MainActivity extends AppCompatActivity {
         viewModel.generateListCoins();
 
     }
+
+
 }
---
+++ app/src/main/java/com/example/coinranking_app/RecyclerAdapterCoin.java
@@ -0,0 +1,55 @@
+package com.example.coinranking_app;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.coinranking_app.models.Coin;
+
+import java.util.List;
+
+public class RecyclerAdapterCoin extends RecyclerView.Adapter<RecyclerAdapterCoin.MyViewHolder>{
+
+    private List<Coin> coin_list;
+
+    public RecyclerAdapterCoin(List<Coin> coinList) {
+        this.coin_list = coinList;
+    }
+
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_coin, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Coin coin = coin_list.get(position);
+        holder.bind(coin);
+    }
+
+    public int getItemCount(){
+        return coin_list.size();
+    }
+
+    public void setCoinList(List<Coin> coins) {
+        this.coin_list = coins;
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView textview_name;
+        private TextView textview_price;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            textview_name = itemView.findViewById(R.id.textview_name);
+            textview_price = itemView.findViewById(R.id.textview_price);
+        }
+
+        public void bind(Coin coin) {
+            textview_name.setText(coin.getName());
+            textview_price.setText(String.valueOf(coin.getPrice()));
+        }
+    }
+}
