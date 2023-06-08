abbdc6f01793faed67b6b9472b25d50b588f7520
msarlat <meven.sarlat@capgemini.com>
2023-06-08 08:29:40 +0200

    feature(detailactivity) : add return button

--- app/src/main/java/com/example/coinranking_app/DetailsActivity.java
+++ app/src/main/java/com/example/coinranking_app/DetailsActivity.java
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModelProvider;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.coinranking_app.databinding.ActivityDetailsBinding;
 import com.example.coinranking_app.models.Coin;
@@ -18,6 +19,8 @@ public class DetailsActivity extends AppCompatActivity {
 
     private ActivityDetailsBinding binding;
     private IDetailsVewModel viewModel;
+
+    private Button button;
 
     private String uuid;
     private Coin coin;
@@ -44,6 +47,15 @@ public class DetailsActivity extends AppCompatActivity {
                     binding.textViewRank.setText("Rank : " + coin.getCoin().getRank());
                     binding.textViewDescription.setText(coin.getCoin().getDescription());
                 }
+            }
+        });
+
+        button = findViewById(R.id.button_back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailsActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
